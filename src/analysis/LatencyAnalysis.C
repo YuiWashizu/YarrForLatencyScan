2c26e0b51a393c7d7eb5d1431fb31c411bfbaaf6
ywashizu <yui.washizu@cern.ch>
2019-11-06 01:09:22 +0900

    add

--- src/analysis/LatencyAnalysis.C
+++ src/analysis/LatencyAnalysis.C
@@ -16,16 +16,18 @@
 #include <iostream>
 
 void LatencyAnalysis(std::string fname) {
-  std::string fout_name = "./analysis/rootfileLatency.root";
-  std::string fpng_name = "./analysis/LatencyScane.png";
+  std::string str = fname.c_str();
+  str.erase(fname.end()-4, fname.end());
+  std::string foutname = str + "_latency.root";
+  // std::string fpng_name = "./analysis/LatencyScane.png";
   TFile* f = TFile::Open(fname.c_str(), "READ");
   TTree* t = dynamic_cast<TTree*>(f->Get("HitTree"));
   fromHitTree event(t);
   int ievent;
   long long int nevents = t->GetEntries();
   int ihit;
-  TFile* fout = TFile::Open(fout_name.c_str(), "RECREATE");
-  TH1* latency = new TH1F("Latency Scan", "HitTree;LatencyConfig;events", 510, 0, 510);
+  TFile* fout = TFile::Open(foutname.c_str(), "RECREATE");
+  TH1* latency = new TH1F("latency", "HitTree;LatencyConfig;events", 510, 0, 510);
   
   for (ievent=0; ievent<nevents; ievent++) { 
     event.GetEntry(ievent);
@@ -37,12 +39,8 @@ void LatencyAnalysis(std::string fname) {
     }
   }
   fout->Write();
-  std::string command = "mv " + fout_name + " " + fname + "latency.root";
-  system(command.c_str());
 
   TCanvas* c1 = new TCanvas("c1", "c1", 700, 500);
   latency->Draw();
-  c1->SaveAs(fpng_name.c_str());
-  std::string command2 = "mv " + fpng_name + " " + fname + ".png";
-  system(command2.c_str());
+  // c1->SaveAs("LatencyScan.png");
 }
